0a79b569254ecc7f7acd907f9e76fd1e25066f46
iizuka2019 <49476247+iizuka2019@users.noreply.github.com>
2019-07-17 07:57:48 +0900

    Update liff.js

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -19,7 +19,7 @@ window.onload = function (e) {
     // init で初期化。基本情報を取得。
     // https://developers.line.me/ja/reference/liff/#initialize-liff-app
     liff.init(function (data) {
-        getProfile();
+        //getProfile();
         initializeApp(data);
     });
 
